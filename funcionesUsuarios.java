0b1b6dd3ee966a652631d4dc3485f096b950d46c
Juan Pablo <juan.poittevin@celltrak.com>
2020-04-02 19:24:15 -0300

    Added funciones usuario and calcular impuestos

---
+++ funcionesUsuarios.java
@@ -0,0 +1,21 @@
+public class funcionesUsuario {
+
+
+    public int calcularImpuestos(Usuario usuario){
+        int discount = 0;
+        if (usuario.getEdad() < 18){
+            discount = 0;
+        } else if (usuario.getEdad() > 18 && usuario.getEdad < 30){
+            discount = 2000;
+        } else if (usuario.getEdad() > 30 && usuario.getEdad < 65){
+            discount = 7000;
+        } else {
+            discount = 2000;
+        }
+        discount = discount - (dicount * ( 0.03 * usuario.getCantHijos() ));
+        if (dicount > (usuario.getSaldo() / 2) ){
+            discount = usuario.getSaldo() / 2
+        }
+        return discount;
+    }
+}
