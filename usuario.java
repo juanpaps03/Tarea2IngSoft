3c7a1a519d8ff07096496e7cad3b1c0b4082f959
Juan Pablo <juan.poittevin@celltrak.com>
2020-04-02 19:18:49 -0300

    Added Getter and Setters

--- usuario.java
+++ usuario.java
@@ -6,4 +6,52 @@ public class Usuario {
     int cantHijos;
     int saldo;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getCantHijos() {
+        return cantHijos;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setCantHijos(int cantHijos) {
+        this.cantHijos = cantHijos;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
 }
